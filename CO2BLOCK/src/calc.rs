da58b78dab7e002f7c70cd950a5754ee085f8752
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-07-31 19:24:13 +0100

    Update 2 files

--- CO2BLOCK/src/calc.rs
+++ CO2BLOCK/src/calc.rs
@@ -1,4 +1,11 @@
-use uom::si::{f64::Length, f64::Ratio};
+use std::num::NonZeroU64;
+
+use uom::si::{
+    f64::{Length, Ratio},
+    length::meter,
+    mass_rate::ton_per_day,
+    time::{day, year},
+};
 
 #[allow(non_snake_case)]
 pub fn nordbotten_impl(r: Length, R: Length, psi: Length, R_ext: Length, gamma: Ratio) -> Ratio {
@@ -34,12 +41,53 @@ struct Args {
     correction: Correction,
     inter_well_dist_min: Len<kilometer>,
     inter_well_dist_max: Option<Len<kilometer>>,
+    num_distances: NonZeroU64,
+    num_wells_max: Option<NonZeroU64>,
+    well_radius: Len<meter>,
+    duration_injection: Time<year>,
+    rate_max: MegatonsPerYear,
+}
+
+struct MegatonsPerYear {
+    value: uom::si::f64::MassRate,
+}
+
+impl MegatonsPerYear {
+    fn new(value: f64) -> Self {
+        let day_to_year = uom::si::f64::Time::new::<year>(1.).get::<day>();
+        MegatonsPerYear {
+            value: uom::si::f64::MassRate::new::<ton_per_day>(value * prefix!(mega) / day_to_year),
+        }
+    }
+
+    fn get(self) -> uom::si::f64::MassRate {
+        self.value
+    }
+}
+
+struct Time<Unit> {
+    value: uom::si::f64::Time,
+    _unit: std::marker::PhantomData<Unit>,
+}
+
+impl<Unit: uom::si::time::Unit + uom::Conversion<f64, T = f64>> Time<Unit> {
+    fn new(value: f64) -> Self {
+        Time {
+            value: uom::si::f64::Time::new::<Unit>(value),
+            _unit: std::marker::PhantomData,
+        }
+    }
+
+    fn get(self) -> uom::si::f64::Time {
+        self.value
+    }
 }
 
 struct Len<Unit> {
     value: Length,
     _unit: std::marker::PhantomData<Unit>,
 }
+
 impl<Unit: uom::si::length::Unit + uom::Conversion<f64, T = f64>> Len<Unit> {
     fn new(value: f64) -> Self {
         Len {
--- CO2BLOCK/src/lib.rs
+++ CO2BLOCK/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate uom;
+
 mod calc;
 mod mex;
 pub use mex::nordbotten;
