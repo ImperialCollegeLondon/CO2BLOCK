5550ca04662d5debfff86fcd431912cbbdab106f
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-08-10 00:00:53 +0100

    Update 2 files

--- CO2BLOCK/src/main.rs
+++ CO2BLOCK/src/main.rs
@@ -1,12 +1,4 @@
-use co2block::calc::{InputReservoirParams, ReservoirParams, default_props};
-use uom::si::{
-    dynamic_viscosity::{DynamicViscosity, pascal_second},
-    f64::{Length, MassDensity, Pressure, ThermodynamicTemperature},
-    length::meter,
-    mass_density::kilogram_per_cubic_meter,
-    pressure::{bar, megapascal},
-    thermodynamic_temperature::degree_celsius,
-};
+use co2block::calc::{InputReservoirParams, ReservoirParams};
 
 fn main() -> anyhow::Result<()> {
     let input: InputReservoirParams = csv::Reader::from_path("input-template.csv")?
