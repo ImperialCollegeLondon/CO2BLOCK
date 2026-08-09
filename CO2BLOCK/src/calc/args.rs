256ef3c0bd57092d2e9705e4711cb1f5bc158be6
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-08-09 14:04:56 +0100

    Update args.rs

--- CO2BLOCK/src/calc/args.rs
+++ CO2BLOCK/src/calc/args.rs
@@ -5,12 +5,16 @@ use uom::si::{
     compressibility_coefficient::per_megapascal,
     f64::*,
     length::kilometer,
-    pressure::megapascal,
+    pressure::{megapascal, pascal},
     ratio::{part_per_million, ratio},
     temperature_gradient::kelvin_per_kilometer,
+    thermodynamic_temperature::degree_celsius,
 };
 
-use crate::calc::PressureGradient;
+use crate::calc::{
+    PressureGradient,
+    eos::{self, gas_density, gas_viscosity},
+};
 
 pub struct Args {
     pub reservoir: ReservoirParams,
@@ -29,7 +33,6 @@ pub struct ReservoirParams {
 
 pub struct WaterParams {
     pub compress: CompressibilityCoefficient,
-    pub density: MassDensity,
     pub visc: DynamicViscosity,
 }
 
@@ -53,12 +56,6 @@ pub struct RockParams {
 pub struct DomainParams {
     pub thickness: Length,
     pub area: Area,
-    pub domain_type: DomainType,
-}
-
-pub enum DomainType {
-    Open,
-    Closed,
 }
 
 pub enum Correction {
@@ -79,64 +76,130 @@ pub struct WellPlaceParams {
     pub num_wells_max: Option<NonZeroU64>,
 }
 
-pub trait DefaultProps {
-    fn litho_grad() -> PressureGradient {
+mod default_props {
+    use super::*;
+    pub(crate) fn litho_grad() -> PressureGradient {
         Pressure::new::<megapascal>(23.) / Length::new::<kilometer>(1.)
     }
 
-    fn hydro_grad() -> PressureGradient {
+    pub(crate) fn hydro_grad() -> PressureGradient {
         Pressure::new::<megapascal>(10.) / Length::new::<kilometer>(1.)
     }
 
-    fn temperature_grad() -> TemperatureGradient {
+    pub(crate) fn temperature_grad() -> TemperatureGradient {
         TemperatureGradient::new::<kelvin_per_kilometer>(33.)
     }
 
-    fn stress_ratio() -> Ratio {
+    pub(crate) fn stress_ratio() -> Ratio {
         Ratio::new::<ratio>(0.7)
     }
 
-    fn rock_friction_angle() -> Angle {
+    pub(crate) fn rock_friction_angle() -> Angle {
         Angle::new::<degree>(30.)
     }
 
-    fn rock_cohesion() -> Pressure {
+    pub(crate) fn rock_cohesion() -> Pressure {
         Zero::zero()
     }
 
-    fn rock_compressibility() -> CompressibilityCoefficient {
+    pub(crate) fn rock_compressibility() -> CompressibilityCoefficient {
         CompressibilityCoefficient::new::<per_megapascal>(5e-4)
     }
 
-    fn water_compressibility() -> CompressibilityCoefficient {
+    pub(crate) fn water_compressibility() -> CompressibilityCoefficient {
         CompressibilityCoefficient::new::<per_megapascal>(3e-4)
     }
 
-    fn salinity() -> Ratio {
+    pub(crate) fn salinity() -> Ratio {
         Ratio::new::<part_per_million>(18e4)
     }
 }
 
-struct InputReservoirProps {
-    shallowest_depth: Length,
-    mean_depth : Length,
-    thickness : Length,
-    area: Area,
-    permeability: Area,
-    porosity: Ratio,
-    rock_compress: CompressibilityCoefficient,
-    water_compress: CompressibilityCoefficient,
-    co2_density : MassDensity,
-    co2_viscosity: DynamicViscosity,
-    water_viscosity: DynamicViscosity,
-    pressure_top: Pressure,
-    pres0 = double(data{site_no,14});                               % pressure at the top of the reservoir [MPa]
-    pres0_mean = double(data{site_no,15});                          % pressure at the centre of the reservoir [MPa]
-    T0_mean = double(data{site_no,16});                             % temperature at the centre of the reservoir [C]
-    salinity = double(data{site_no,17})/1e6;                        % aquifer salinity [ppm/1e6]
-    s1_tot = double(data{site_no,18});                              % total maximum principal stress at the reservoir [MPa]
-    stress_ratio = double(data{site_no,19});                        % ratio of principal effective stresses (s3/s1) [-]
-    friction = double(data{site_no,20});                            % rock friction angle  [deg]
-    cohesion = double(data{site_no,21});                            % rock cohesion coefficient [MPa]
-    tens_strength = double(data{site_no,22});                       % rock tensile strength [MPa]
+pub struct InputReservoirParams {
+    pub shallowest_depth: Length,
+    pub mean_depth: Length,
+    pub thickness: Length,
+    pub area: Area,
+    pub permeability: Area,
+    pub porosity: Ratio,
+    pub rock_compress: Option<CompressibilityCoefficient>,
+    pub water_compress: Option<CompressibilityCoefficient>,
+    pub pressure_top: Option<Pressure>,
+    pub pressure_center: Option<Pressure>,
+    pub co2_density: Option<MassDensity>,
+    pub co2_viscosity: Option<DynamicViscosity>,
+    pub water_viscosity: Option<DynamicViscosity>,
+    pub salinity: Option<Ratio>,
+    pub temperature_center: Option<ThermodynamicTemperature>,
+    pub total_max_princ_stress: Option<Pressure>,
+    pub princ_stress_ratio: Option<Ratio>,
+    pub rock_friction_angle: Option<Angle>,
+    pub rock_cohesion: Option<Pressure>,
+    pub rock_tensile_strength: Option<Pressure>,
+}
+
+impl From<InputReservoirParams> for ReservoirParams {
+    fn from(input: InputReservoirParams) -> Self {
+        let domain = DomainParams {
+            thickness: input.thickness,
+            area: input.area,
+        };
+        let cohesion = input
+            .rock_cohesion
+            .unwrap_or(default_props::rock_cohesion());
+        let rock = RockParams {
+            porosity: input.porosity,
+            permeability: input.permeability,
+            compress_rock: input
+                .rock_compress
+                .unwrap_or(default_props::rock_compressibility()),
+            cohesion,
+            stress_ratio: input
+                .princ_stress_ratio
+                .unwrap_or(default_props::stress_ratio()),
+            friction_angle: input
+                .rock_friction_angle
+                .unwrap_or(default_props::rock_friction_angle()),
+            tensile_strength: input.rock_tensile_strength.unwrap_or(cohesion / 2.),
+            max_principal_stress: input
+                .total_max_princ_stress
+                .unwrap_or(default_props::litho_grad() * input.mean_depth),
+            top_pressure: input
+                .pressure_top
+                .unwrap_or(default_props::hydro_grad() * input.mean_depth),
+        };
+
+        let temperature = input.temperature_center.unwrap_or(
+            ThermodynamicTemperature::new::<degree_celsius>(15.)
+                + default_props::temperature_grad() * input.mean_depth,
+        );
+
+        let salinity = input.salinity.unwrap_or(default_props::salinity());
+
+        let water = WaterParams {
+            compress: input
+                .water_compress
+                .unwrap_or(default_props::water_compressibility()),
+            visc: input
+                .water_viscosity
+                .unwrap_or(eos::brine_viscosity(temperature, salinity)),
+        };
+
+        let pressure = input
+            .pressure_center
+            .unwrap_or(default_props::hydro_grad() * input.mean_depth);
+
+        let gas_density = gas_density(pressure, temperature);
+        let gas = GasParams {
+            density: gas_density,
+            visc: gas_viscosity(pressure, temperature, gas_density),
+        };
+
+        ReservoirParams {
+            domain,
+            rock,
+            water,
+            gas,
+        }
+    }
 }
