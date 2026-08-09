da4ff449f13244b8b2449e8779b593fa49df9885
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-08-10 00:09:08 +0100

    Update main.rs

--- CO2BLOCK/src/main.rs
+++ CO2BLOCK/src/main.rs
@@ -1,7 +1,11 @@
 use std::str::FromStr;
 
-use co2block::calc::{InjectionParams, InputReservoirParams, ReservoirParams};
-use uom::si::f64::{Length, MassRate, Time};
+use co2block::calc::{InjectionParams, InputReservoirParams, ReservoirParams, megaton_per_year};
+use uom::si::{
+    f64::{Length, MassRate, Time},
+    length::{kilometer, meter},
+    time::year,
+};
 
 fn main() -> anyhow::Result<()> {
     let input: InputReservoirParams = csv::Reader::from_path("input-template.csv")?
@@ -10,14 +14,14 @@ fn main() -> anyhow::Result<()> {
         .next()
         .ok_or(anyhow::anyhow!("Empty input"))??;
     let reservoir: ReservoirParams = input.into();
-    println!("{reservoir:#?}");
+    // println!("{reservoir:#?}");
 
-    let step = Length::from_str("1 km")?;
+    let step = Length::new::<kilometer>(2.);
 
     let injection = InjectionParams {
-        well_radius: Length::from_str("0.2 m")?,
-        duration: Time::from_str("100 y")?,
-        max_well_rate: MassRate::from_str("20 Mt/y")?,
+        well_radius: Length::new::<meter>(0.2),
+        duration: Time::new::<year>(100.),
+        max_well_rate: MassRate::new::<megaton_per_year>(20.),
     };
 
     co2block::calc::co2block_with_placement(
