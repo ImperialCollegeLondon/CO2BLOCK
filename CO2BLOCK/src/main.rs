fd1750eccd882951e5e863d69fc62efad831ea09
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-08-09 16:07:06 +0100

    Update main.rs

--- CO2BLOCK/src/main.rs
+++ CO2BLOCK/src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{io, str::FromStr};
 
 use co2block::calc::InputReservoirParams;
 use uom::si::f64::Length;
@@ -7,4 +7,6 @@ fn main() {
     let data = InputReservoirParams::default();
     let json = serde_json::to_string_pretty(&data).unwrap();
     println!("{json}");
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+    wtr.serialize(data).unwrap()
 }
